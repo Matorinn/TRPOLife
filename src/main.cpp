fe2fd74a2d9bc33be1dfbc564b4d6257d4d9cd37
1Vasiliy <cagefighter565@gmail.com>
2019-05-12 19:37:20 +0700

    Created an empty window that closes when you press ESC

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,40 @@
+#include "GL/gl.h"
+#include "GL/glut.h"
 #include <iostream>
-main(int argc, char* argv[])
+
+int width = 1200;
+int height = 600;
+
+void draw();
+void keyboard(unsigned char key, int x, int y);
+
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!" << '\n';
+    glutInit(&argc, argv);
+    glutInitWindowSize(width, height);
+    glutInitWindowPosition(100, 100);
+
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutCreateWindow("Game of life");
+
+    glutDisplayFunc(draw);
+    glutKeyboardFunc(keyboard);
+    glClearColor(0, 0, 0, 0);
+
+    glutMainLoop();
+
+    return 0;
+}
+
+void draw()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+    // 27 = ESC
+    if (key == 27)
+        exit(0);
 }
