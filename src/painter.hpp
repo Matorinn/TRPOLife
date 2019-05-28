2c52e71caec0e72930704d7ec5972b79aea013fe
Mato <dany.lesckovetz@gmail.com>
2019-05-28 16:16:15 +0700

    Add color palette

--- src/painter.hpp
+++ src/painter.hpp
@@ -2,5 +2,7 @@
 
 class Painter {
 public:
+    enum Color { RED, WHITE, MAGENTA, DARK_BLUE, GREEN, BROWN, CYAN, BLACK };
     void rect(int x1, int y1, int x2, int y2);
+    void setColor(Color);
 };
