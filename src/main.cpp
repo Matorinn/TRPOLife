ecf22d7e222f0d9ab2a8f024e14b4ee2d35b8a83
Mato <dany.lesckovetz@gmail.com>
2019-07-01 00:23:13 +0700

    Add play button and textures

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "painter.hpp"
 #include <GL/gl.h>
 #include <GL/glut.h>
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
 unsigned char lastKey = 0;
@@ -12,7 +13,7 @@ void keyboard(unsigned char key, int x, int y);
 
 Game game;
 
-int main(int argc, char* argv[])
+int gamedraw(int argc, char** argv)
 {
     glutInit(&argc, argv);
     glutInitWindowSize(Field::WIDTH * Field::SIZE, Field::HEIGHT * Field::SIZE);
@@ -38,6 +39,48 @@ int main(int argc, char* argv[])
     glutMainLoop();
 
     return 0;
+}
+
+int main(int argc, char* argv[])
+{
+    sf::RenderWindow window(sf::VideoMode(1000, 600), "MainMenu");
+    sf::Texture pb, rb, ob, xb, bb, ttle;
+    ttle.loadFromFile("textures/title.png");
+    pb.loadFromFile("textures/play.png");
+    rb.loadFromFile("textures/rules.png");
+    ob.loadFromFile("textures/options.png");
+    xb.loadFromFile("textures/exit.png");
+    bb.loadFromFile("textures/back.png");
+    sf::Sprite playb, rulesb, optionsb, exitb, backb, title;
+    playb.setTexture(pb);
+    rulesb.setTexture(rb);
+    optionsb.setTexture(ob);
+    exitb.setTexture(xb);
+    backb.setTexture(bb);
+    title.setTexture(ttle);
+
+    sf::Event event;
+    while (window.isOpen()) {
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+
+            if (event.type == sf::Event::MouseButtonReleased
+                && event.mouseButton.button == sf::Mouse::Left) {
+                if (sf::IntRect(playb.getGlobalBounds())
+                            .contains(sf::Mouse::getPosition(window))) {
+                    gamedraw(argc, argv);
+                }
+            }
+        }
+
+        window.clear(sf::Color(82, 82, 82, 255));
+        window.draw(title);
+        title.setPosition(300, 0);
+        window.draw(playb);
+        playb.setPosition(300, 125);
+        window.display();
+    }
 }
 
 void keyboard(unsigned char key, int x, int y)
